4c812d301681690b8b7c316680be0ce58a45efa8
Zilong ZHAO <zilong@mirohealth.com>
2020-09-25 22:43:21 +0200

    bash to run test

--- result_test.sh
+++ result_test.sh
@@ -1,3 +1,6 @@
+python3  federated_learning_inputation.py 0.0 [] > result_inputation_0_0.txt
+python3  federated_learning_reputation.py 0.0 [] > result_reputation_0_0.txt
+python3  federated_learning_shapley.py 0.0 []  > result_shapley_0_0.txt
 for attacker in [1] [1,2] [1,2,3]
 do
   for noise in 0.1 0.3 0.5 0.7 1
